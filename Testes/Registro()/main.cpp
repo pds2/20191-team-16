4483fa1308604c10621dab7bfe7496b914431142
Willian-Build <48255759+Willian-Build@users.noreply.github.com>
2019-05-13 17:19:00 -0300

    Add files via upload

---
+++ Testes/Registro()/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "registro_01.h"
+
+int main()
+{
+    system("clear");
+
+    std::cout << "-Teste com funcao de registro-" << std::endl;
+
+    registro();
+
+    return 0;
+}
