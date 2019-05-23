efe1bc8685572d69cbbda8025a237f587584ba53
Willian-Build <48255759+Willian-Build@users.noreply.github.com>
2019-05-23 17:05:38 -0300

    Upload temporario de arquivos cpp

---
+++ mail 15052019/src/mensagem_cvt.cpp
@@ -0,0 +1,26 @@
+#include "mensagem_cvt.h"
+
+#include <fstream>
+
+//Construtor
+Mensagem_cvt::Mensagem_cvt(std::fstream email)
+{
+	//Place Holder
+}
+
+//Destrutor
+Mensagem_cvt::~Mensagem_cvt()
+{
+	//Place Holder
+}
+
+//Metodos
+void Mensagem_cvt::exibir_msg()
+{
+	//Place Holder
+}
+
+void Mensagem_cvt::escrever_msg()
+{
+	//Place Holder
+}
---
+++ mail 15052019/src/mensagem_gnr.cpp
@@ -0,0 +1,27 @@
+#include "mensagem_gnr.h"
+
+#include <fstream>
+
+//Construtor
+Mensagem_gnr::Mensagem_gnr(std::fstream email)
+{
+	//Place Holder
+}
+
+//Destrutor
+Mensagem_gnr::~Mensagem_gnr()
+{
+	//Place Holder
+}
+
+//Metodos
+void Mensagem_cvt::exibir_msg()
+{
+	//Place Holder
+}
+
+void Mensagem_cvt::escrever_msg()
+{
+	//Place Holder
+}
+
