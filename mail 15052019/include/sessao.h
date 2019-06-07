c134f986f9362c1540d861592c74aedd79821f14
Willian-Build <48255759+Willian-Build@users.noreply.github.com>
2019-06-07 08:15:35 -0300

    Update do sessao.h

--- mail 15052019/include/sessao.h
+++ mail 15052019/include/sessao.h
@@ -10,13 +10,23 @@ class Sessao {
 
 private:
 
+    /*Variavel com o nome do usuario. Usado como referencia para
+    carregar os arquivos de sua pasta na memoria*/
     std::string _nome;
+
+    //Variavel que armazena a senha do usuario, usada para configuracao da conta
     std::string _senha;
 
     //Interruptor que define como a caixa de emails deve ser organizada
     short _boxflag;
 
-    std::vector <Mensagem> _box;
+    /*Vetores que armazenam todas as mensagens carregadas na memoria,
+    por sub classe*/
+    std::vector <Mensagem_gnr> _box0;
+    std::vector <Mensagem_lst> _box1;
+    std::vector <Mensagem_cvt> _box2;
+    std::vector <Mensagem_tab> _box3;
+    std::vector <Mensagem_img> _box4;
     
     //Submetodos
     void limpa_buffer();
@@ -24,6 +34,23 @@ private:
     void set_user(std::string nome, std::string nome2);
     void set_password(std::string nome, std::string senha);
     void del_conta(std::string nome);
+    
+    bool filtro_msg(Mensagem &msg);
+    void add_msg_time(long data);
+    void achar_msg(bool &okflag, long timepos, Mensagem &box);
+    
+    void add_msg_gnr(std::string file , short lido, long data, std::string remet,
+                     std::string desti, std::string assun, std::fstream &read_target);
+    void add_msg_lst(std::string file , short lido, long data, std::string remet,
+                     std::string desti, std::string assun, std::fstream &read_target);
+    void add_msg_cvt(std::string file , short lido, long data, std::string remet,
+                     std::string desti, std::string assun, std::fstream &read_target);
+    void add_msg_tab(std::string file , short lido, long data, std::string remet,
+                     std::string desti, std::string assun, std::fstream &read_target);
+    void add_msg_img(std::string file , short lido, long data, std::string remet,
+                     std::string desti, std::string assun, std::fstream &read_target);
+    std::string string_data(long data);
+    
 
 public:
 
@@ -46,9 +73,8 @@ public:
     void set_boxflag(short boxflag);
 
     //Metodos
-    void clear_box();
+    void limpar_box();
     void listar_box();
-
     void config_conta();
 };
 
