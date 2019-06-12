2717fca2ba7ffbb67b62b90d6b536a494556fc08
leoalmeidab <48333675+leoalmeidab@users.noreply.github.com>
2019-06-12 14:40:12 -0300

    Update sessao.h

--- mail 15052019/include/sessao.h
+++ mail 15052019/include/sessao.h
@@ -31,9 +31,13 @@ private:
     //Submetodos
     void limpa_buffer();
     bool match_log_(std::string nome2);
-    void set_user(std::string nome, std::string nome2);
-    void set_password(std::string nome, std::string senha);
-    void del_conta(std::string nome);
+    void set_user(std::string nome2);
+    void set_password(std::string senha);
+    void del_conta();
+    
+    void op1_alt_nome (std::string nome2);
+    void op2_alt_senha(std::string senha);
+    void op3_del_conta ();
     
     bool filtro_msg(Mensagem &msg);
     void add_msg_time(long data);
